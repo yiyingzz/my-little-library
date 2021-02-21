bd2475857c7d71aade3a561de90179c8fde31728
Yiying Zou <yiyingzou@gmail.com>
2021-02-20 21:24:20 -0500

    styling + refactor some js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let library = [];
+let activeView = ""; // default is all for desktop, reading for mobile
+
+const form = document.querySelector("form");
 
 //set up book class
 function Book(title, author, pages, isRead) {
@@ -42,24 +45,44 @@ function displayBooks() {
   // loop through library obj
   library.forEach((book, index) => {
     let ul = "";
-    readInfo = "";
+    let isRead = false;
+    // let progressCSS = "card__border--reading";
     if (book.isRead) {
-      readInfo = "read";
+      isRead = true;
+      // progressCSS = "card__border--read";
       ul = document.getElementById("read");
     } else {
-      readInfo = "in progress";
       ul = document.getElementById("reading");
     }
     // build html
-    let html = `
-        <h2 class="card__title">${book.title}</h2>
-        <p class="card__author">${book.author}</p>
-        <p class="card__pages">${book.pages} pages</p>
-        <p class="card__progress">${readInfo}</p>
-      `;
+    // let html = `
+
+    //     <h2 class="card__title">${book.title}</h2>
+    //     <p class="card__author">${book.author}</p>
+    //     <p class="card__pages">${book.pages} pages</p>
+    //     <p class="card__progress">${readInfo}</p>
+    //   `;
+
+    // let html = `
+    //   <div class="card__border ${progressCSS}"></div>
+    //     <div class="card__heading">
+    //       <h2 class="card__title">${book.title}</h2>
+    //       <div class="card__edit"></div>
+    //     </div>
+    //     <p class="card__author">${book.author}</p>
+    //     <p class="card__pages">${book.pages} pages</p>
+    //     <p class="card__progress">${isRead}</p>
+    //   `;
     const li = document.createElement("li");
     li.setAttribute("class", "card");
     li.setAttribute("id", index);
+    const border = document.createElement("div");
+    if (isRead) {
+      border.setAttribute("class", "card__border card__border--read");
+    } else {
+      border.setAttribute("class", "card__border card__border--reading");
+    }
+    li.append(border);
     const h2 = document.createElement("h2");
     h2.setAttribute("class", "card__title");
     h2.innerText = book.title;
@@ -70,11 +93,15 @@ function displayBooks() {
     li.append(pAuthor);
     const pPages = document.createElement("p");
     pPages.setAttribute("class", "card__pages");
-    pPages.innerText = book.pages + " pages";
+    pPages.innerText = "Length: " + book.pages + " pages";
     li.append(pPages);
     const pProgress = document.createElement("p");
     pProgress.setAttribute("class", "card__progress");
-    pProgress.innerText = readInfo;
+    if (isRead) {
+      pProgress.innerText = "Status: Read";
+    } else {
+      pProgress.innerText = "Status: Reading";
+    }
     li.append(pProgress);
     ul.append(li);
   });
@@ -99,33 +126,56 @@ function attachToggleReadEventListener() {
 }
 
 // rewrite below as one named function that is an evt listener, attach to all 3 nav links, function needs to take in parameter (event, use event target class/id to decide what to show/hide)
-document.querySelector(".nav__reading").addEventListener("click", function () {
-  this.classList.add("active");
-  document.querySelector(".nav__read").classList.remove("active");
-  document.querySelector(".nav__add").classList.remove("active");
-  document.getElementById("reading").style.display = "block";
-  document.getElementById("read").style.display = "none";
-  document.querySelector(".form").style.display = "none";
-});
+// this is the event listener
+function determineView(e) {
+  if (e.target.dataset.view === "add") {
+    toggleForm();
+  } else {
+    showCategory(e.target.dataset.view);
+    activeView = e.target;
+  }
+  const nav = document.getElementsByClassName("nav__link");
+  for (let item of nav) {
+    item.classList.remove("active");
+  }
+  e.target.classList.add("active");
+}
 
-document.querySelector(".nav__read").addEventListener("click", function () {
-  this.classList.add("active");
-  document.querySelector(".nav__reading").classList.remove("active");
-  document.querySelector(".nav__add").classList.remove("active");
-  document.getElementById("read").style.display = "block";
-  document.getElementById("reading").style.display = "none";
-  document.querySelector(".form").style.display = "none";
-});
+// this should be a function that runs through the library array and displays the matching category, so the default will always be "all"
+// this also prevents having the categories split when there are multiple (so just have one <ul> to catch everything, and use the nav links as filters, instead of 3 different <ul>?)
+function showCategory(category) {
+  const categories = document.getElementsByClassName("category");
+  for (let item of categories) {
+    item.style.display = "none";
+  }
+  form.style.display = "none";
+  document.getElementById(category).style.display = "grid";
+}
 
-document.querySelector(".nav__add").addEventListener("click", function () {
-  this.classList.add("active");
-  document.querySelector(".nav__read").classList.remove("active");
-  document.querySelector(".nav__reading").classList.remove("active");
-  document.querySelector(".form").style.display = "block";
-});
+function toggleForm() {
+  const form = document.querySelector("form");
+  if (form.style.display === "block") {
+    form.style.display = "none";
+    document.querySelector(".nav__link--add").classList.remove("active");
+    activeView.classList.add("active");
+  } else {
+    form.style.display = "block";
+  }
+}
 
 //onload
 document.addEventListener("DOMContentLoaded", function () {
+  /* event listeners */
+  const navLinks = document.querySelectorAll(".nav__link");
+  for (let link of navLinks) {
+    link.addEventListener("click", function (e) {
+      determineView(e);
+    });
+  }
+
+  document.querySelector(".form__close").addEventListener("click", toggleForm);
+
+  /* check for stored data */
   if (0 < localStorage.length) {
     libraryData = JSON.parse(localStorage.getItem("books"));
     library = libraryData.map((item) => {
@@ -133,6 +183,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return book;
     });
     displayBooks();
+    // set activeView here (default on load)
   }
 });
 
@@ -140,3 +191,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // need form error handling
 // if title & author exact match for something already in library[], "do you already have this book on your list?" --> parse title & author in lower case to check for matches
 // add delete button to delete books
+// add edit button for editing book info
