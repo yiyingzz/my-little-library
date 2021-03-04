46c553d3f043ecb05b95d4a4cc3d29c240175fad
Yiying Zou <yiyingzou@gmail.com>
2021-03-03 22:36:33 -0500

    delete books function added + styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let library = [];
-let activeView = ""; // default is all for desktop, reading for mobile
+let activeView = "";
+let activeCategory = "all";
 
 const list = document.querySelector("#booklist");
 const form = document.querySelector("form");
@@ -7,6 +8,7 @@ const formLink = document.querySelector(".nav__link--add");
 const readingLink = document.querySelector(".nav__link--reading");
 const readLink = document.querySelector(".nav__link--read");
 const allLink = document.querySelector(".nav__link--all");
+const emptyList = document.querySelector("#empty");
 
 //set up book class
 function Book(title, author, pages, isRead) {
@@ -14,15 +16,13 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+  //this.id = id;
 }
 
 Book.prototype = {
   constructor: Book,
   toggleRead: function () {
     this.isRead = !this.isRead;
-  },
-  deleteBook: function () {
-    console.log("delete");
   }
 };
 
@@ -31,10 +31,11 @@ function addBookToLibrary() {
   const author = document.querySelector('input[name="author"]').value;
   const pages = parseInt(document.querySelector('input[name="pages"]').value);
   const isRead = document.querySelector('input[name="progress"]').checked;
+  //const id = idCount;
   const newBook = new Book(title, author, pages, isRead);
   library.push(newBook);
   localStorage.setItem("books", JSON.stringify(library));
-  displayBooks("all");
+  displayBooks(activeCategory);
 }
 
 function filterBooks(category) {
@@ -60,56 +61,96 @@ function filterBooks(category) {
 function displayBooks(category) {
   list.innerHTML = "";
 
-  const filteredLibrary = filterBooks(category);
-  filteredLibrary.forEach((book, index) => {
-    const ul = list;
-    let isRead = false;
-    if (book.isRead) {
-      isRead = true;
-    }
-    const li = document.createElement("li");
-    li.setAttribute("class", "card");
-    li.setAttribute("id", index);
-    const border = document.createElement("div");
-    if (isRead) {
-      border.setAttribute("class", "card__border card__border--read");
-    } else {
-      border.setAttribute("class", "card__border card__border--reading");
-    }
-    li.append(border);
-    const h2 = document.createElement("h2");
-    h2.setAttribute("class", "card__title");
-    h2.innerText = book.title;
-    li.append(h2);
-    const pAuthor = document.createElement("p");
-    pAuthor.setAttribute("class", "card__author");
-    pAuthor.innerText = book.author;
-    li.append(pAuthor);
-    const pPages = document.createElement("p");
-    pPages.setAttribute("class", "card__pages");
-    pPages.innerText = "Length: " + book.pages + " pages";
-    li.append(pPages);
-    const pProgress = document.createElement("p");
-    pProgress.setAttribute("class", "card__progress");
-    if (isRead) {
-      pProgress.innerText = "Status: Read";
-    } else {
-      pProgress.innerText = "Status: Reading";
-    }
-    li.append(pProgress);
-    ul.append(li);
-  });
-  attachToggleReadEventListener();
+  if (0 < library.length) {
+    emptyList.style.display = "none";
+    const filteredLibrary = filterBooks(category);
+    filteredLibrary.forEach((book, index) => {
+      const ul = list;
+      let isRead = false;
+      if (book.isRead) {
+        isRead = true;
+      }
+      const li = document.createElement("li");
+      li.setAttribute("class", "card");
+      li.setAttribute("id", index);
+      const border = document.createElement("div");
+      if (isRead) {
+        border.setAttribute("class", "card__border card__border--read");
+      } else {
+        border.setAttribute("class", "card__border card__border--reading");
+      }
+      li.append(border);
+      const h2 = document.createElement("h2");
+      h2.setAttribute("class", "card__title");
+      h2.innerText = book.title;
+      li.append(h2);
+      const pAuthor = document.createElement("p");
+      pAuthor.setAttribute("class", "card__author");
+      pAuthor.innerText = book.author;
+      li.append(pAuthor);
+      const pPages = document.createElement("p");
+      pPages.setAttribute("class", "card__pages");
+      pPages.innerText = "Length: " + book.pages + " pages";
+      li.append(pPages);
+      const divModifyContainer = document.createElement("div");
+      divModifyContainer.setAttribute("class", "card__modify-container");
+      const pProgress = document.createElement("p");
+      pProgress.setAttribute("class", "card__progress");
+      let progressHtml = "Status: ";
+      if (isRead) {
+        progressHtml += `<a href="javascript:void(0);" class="card__progress--update card__progress--update-read">Read</a>`;
+      } else {
+        progressHtml += `<a href="javascript:void(0);" class="card__progress--update">Reading</a>`;
+      }
+      pProgress.innerHTML = progressHtml;
+      const divModify = document.createElement("div");
+      divModify.setAttribute("class", "card__modify");
+      const aDelete = document.createElement("a");
+
+      aDelete.setAttribute("class", "card__modify--link");
+      if (isRead) {
+        aDelete.setAttribute(
+          "class",
+          "card__modify--link card__modify--link-read"
+        );
+      }
+      aDelete.setAttribute("href", "javascript:void(0);");
+      const imgDelete = document.createElement("img");
+      imgDelete.setAttribute("src", "./assets/delete.svg");
+      imgDelete.setAttribute("alt", "");
+      imgDelete.setAttribute("class", "card__modify--icon");
+      aDelete.append(imgDelete);
+      divModifyContainer.append(pProgress);
+      divModifyContainer.append(divModify);
+      divModify.append(aDelete);
+      li.append(divModifyContainer);
+      ul.append(li);
+    });
+    attachToggleReadEventListener();
+  } else {
+    emptyList.style.display = "block";
+  }
 }
 
 function attachToggleReadEventListener() {
   const cards = document.querySelectorAll(".card");
   cards.forEach((card) => {
     card.addEventListener("click", function (e) {
-      if (e.target.className === "card__progress") {
+      console.log("added click");
+      console.log(e.target.className);
+      const progressRegex = /card__progress--update/;
+      const deleteRegex = /card__modify--/;
+      if (progressRegex.test(e.target.className)) {
         library[e.currentTarget.id].toggleRead();
         localStorage.setItem("books", JSON.stringify(library));
-        displayBooks("all");
+        console.log(localStorage);
+        displayBooks(activeCategory);
+      }
+      if (deleteRegex.test(e.target.className)) {
+        library.splice(this.id, 1);
+        localStorage.setItem("books", JSON.stringify(library));
+        console.log(localStorage);
+        displayBooks(activeCategory);
       }
     });
   });
@@ -125,8 +166,9 @@ function determineView(e) {
   if (e.target.dataset.view === "add") {
     toggleForm();
   } else {
-    displayBooks(e.target.dataset.view);
     activeView = e.target;
+    activeCategory = e.target.dataset.view;
+    displayBooks(activeCategory);
   }
 }
 
@@ -165,7 +207,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const book = new Book(item.title, item.author, item.pages, item.isRead);
       return book;
     });
-    displayBooks("all");
+    displayBooks(activeCategory);
     activeView = allLink;
     allLink.classList.add("active");
   }
